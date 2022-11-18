8fbf5f01a1d7b8aece0479756120ed583848d8c9
wovowo <yvarova2020@mail.ru>
2022-11-18 09:16:37 +0300

    yh

---
+++ test3.php
@@ -0,0 +1,34 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <h1>Форма ввода</h1>
+    <form action="/action_page.php">
+    
+        <label for="lname">Фамилия:</label>
+        <input type="text" id="lname" name="lname"><br><br>
+        <label for="fname">Имя:</label>
+        <input type="text" id="fname" name="fname"><br><br>
+        <label for="otch">Отчество:</label>
+        <input type="text" id="otch" name="otch"><br><br>
+        <label for="pass">Пароль:</label>
+        <input type="password" name="pass"><br><br>
+        <label for="date1">Дата рождения</label>
+        <input type="date" name="date1"><br><br>
+        <label for="tel">Телефон</label>
+        <input type="number" name="tel"><br><br>
+
+        <input type="submit" value="Submit">
+</form>
+</div>
+
+</body>
+</html>
