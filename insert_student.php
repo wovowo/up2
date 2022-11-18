0626330090e464821e1a3dfce187a58956cb4640
wovowo <yvarova2020@mail.ru>
2022-11-18 09:36:59 +0300

    kjbh

---
+++ insert_student.php
@@ -0,0 +1,15 @@
+<?php
+require_once("init.php");
+
+$fname=$_GET["fname"];
+$otch=$_GET["otch"]
+$lname=$_GET["lname"];
+$date1=$_GET["date_r"];
+$tel=$_GET["tel"];
+
+$sql = "insert students(fam, name, otch, date_r, tel, city_id, group_id)
+        values('$lname', '$fname', '$otch', '$date_r', '$tel', 1, 1)";
+$result = mysqli_query($db_handler,$sql) or die ("Невозможно выполнить SQL запрос в 'get-student.php'!".mysqli_error($db_handler));
+echo "<h1>HELP</h1>"
+    
+?>
